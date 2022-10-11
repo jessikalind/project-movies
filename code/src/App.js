5450d5c8511dfd75418b30cd4b5b9b8d7abec128
jessikalind <jessika.lind@me.com>
2022-10-11 08:37:27 +0200

    images are displayed

--- code/src/App.js
+++ code/src/App.js
@@ -24,17 +24,14 @@ export const App = () => {
   // }, [])
 
   return (
-    <div>
+    <section className="main-section">
       {movies.map((movie) => (
-        <>
+        <div className="movie-container">
+          <img src={`https://image.tmdb.org/t/p/w300${movie.poster_path}`} alt="film" />
           <h1>{movie.original_title}</h1>
-          <p>{movie.overview}</p>
-          <p>Release date: {movie.release_date}</p>
-          <p>{movie.id}</p>
-          <img src={`https://image.tmdb.org/t/p/w92${movie.poster_path}`} alt="film" />
-        </>
+        </div>
       ))}
-    </div>
+    </section>
   );
 }
 
